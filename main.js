5738169bc72398b72255e077302f3c6d3a6a1e26
Dominique Charlebois <domo.charlebois@gmail.com>
2019-11-25 19:33:25 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const addition = () => {
+  let firstNumber = parseInt(document.getElementById("firstNumber").value);
+  let secondNumber = parseInt(document.getElementById("secondNumber").value);
+
+  let addition = firstNumber + secondNumber;
+
+  alert(addition);
+}
